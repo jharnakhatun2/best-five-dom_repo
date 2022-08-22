e942f4f9f1591fd7ab99b004e1a0581af0a1ce12
Jharna203 <jharnakhatun203@gmail.com>
2022-08-23 05:56:32 +0600

    added Budget js calculation completed

--- js/player.js
+++ js/player.js
@@ -1,2 +1,29 @@
+// Player Total Expenses
 
-    
+document.getElementById('calculate').addEventListener('click', function () {
+    const perPlayerInputField = inputFieldValueGetById('per-player');
+
+    // player Expenses
+    const totalPlayerExpenses = perPlayerInputField * 5;
+    setTextElementValueById('player-expenses', totalPlayerExpenses);
+});
+
+//Calculate Total Expenses
+document.getElementById('calculate-total').addEventListener('click', function () {
+    const playerTotalExpenses = elementTextGetById('player-expenses');
+    const managerExpenses = inputFieldValueGetById('manager-expenses');
+    const coachExpenses = inputFieldValueGetById('coach-expenses');
+    const calculateTotalExpenses = playerTotalExpenses + managerExpenses + coachExpenses;
+    setTextElementValueById('total-management', calculateTotalExpenses);
+});
+
+// Selected-V aside
+const playerArray = [];
+function playerSelection(element){
+    const playerName = element.parentNode.parentNode.children[0].innerText;
+    console.log(playerName);
+    const players = playerName;
+    playerArray.push(players);
+    console.log(playerArray);
+
+}
--- js/utility.js
+++ js/utility.js
@@ -1,7 +1,21 @@
+function inputFieldValueGetById(inputFieldId) {
+    const inputFieldValueById = document.getElementById(inputFieldId);
+    const inputFieldValueString = inputFieldValueById.value;
+    const inputFieldValue = parseFloat(inputFieldValueString);
+    inputFieldValueById.value = '';
+    return inputFieldValue;
+}
+
+function elementTextGetById(elementId) {
+    const elementTextId = document.getElementById(elementId);
+    const elementTextValueString = elementTextId.innerText;
+    const elementTextValue = parseFloat(elementTextValueString);
+    return elementTextValue;
+}
+
+function setTextElementValueById(elementId, newValue) {
+    const textElement = document.getElementById(elementId);
+    textElement.innerText = newValue;
+}
 
 
-    
-    
-    
-    
-   
